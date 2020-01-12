e5ca70675f3487732981b1b95497f5e04d507ce2
Craig Demyanovich <cdemyanovich@gmail.com>
2020-01-11 20:55:53 -0600

    Solve day 2-2

--- src/advent_of_code_2019/day_two_one.clj
+++ src/advent_of_code_2019/day_two_one.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2019.day-two-one
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.math.combinatorics :as combinatorics]
             [clojure.string :as str]))
 
 (def raw-program
@@ -44,9 +45,43 @@
                (nth program (nth program (+ 2 opcode-position)))))
           (+ 4 opcode-position))))))
 
+(def allowable-inputs
+  "Each of the two input values will be between 0 and 99, inclusive."
+  (range 0 100))
+
+(defn combinations-of-inputs
+  "Since our two operations, addition (opcode 1) and multiplication (opcode 2), are
+  commutative (see: https://en.wikipedia.org/wiki/Commutative_property), we produce a
+  selection (see: https://en.wikipedia.org/wiki/Combination) of allowable inputs n
+  at a time."
+  [inputs n]
+  (combinatorics/selections inputs n))
+
+(defn update-program
+  [program [noun verb]]
+  (-> program
+      (assoc 1 noun)
+      (assoc 2 verb)))
+
 (comment
   ;; What value is left at position 0 after the program halts? Answer: 4138687.
   (->> raw-program
        parse-program
        restore-program-to-state-before-fire
        execute))
+
+(comment
+  ;; Find the input noun and verb that cause the program to produce the output 19690720.
+  ;; What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+  ;; Answer: noun=66 and verb=35, or 6635.
+  (let [initial-program (parse-program raw-program)
+        update-program' (partial update-program initial-program)
+        inputs (combinations-of-inputs allowable-inputs 2)
+        possible-programs (map update-program' inputs)
+        candidate-programs (map execute possible-programs)
+        desired-programs (filter #(= 19690720 (first %)) candidate-programs)
+        desired-program (first desired-programs)
+        noun (nth desired-program 1)
+        verb (nth desired-program 2)
+        answer (+ (* 100 noun) verb)]
+    answer))
